c965a06098d0aca10624d6005283db175e2f9e70
dkzeb <sthazeb@gmail.com>
2015-09-28 00:11:20 +0200

    WiiMote controls og korrekt character flip - samt nye walk og idle animationer

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,31 +4,82 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
 
 	private Rigidbody2D rigid;
 	private Animator anim;
 	private Transform playerModel;
+    public bool wiimoteEnabled = false;
+    private int jumpCount = 0;
+
+    private WiiMoteInputReader wiiInput;
+
+
+    private bool facingLeft = false;
+    private bool facingRight = true;
 
 	void Start(){
 		rigid = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
-		playerModel = transform.Find("PlayerModel");
-	}
+        
+        wiiInput = GameObject.FindWithTag("Mechanics").GetComponentInChildren<WiiMoteInputReader>();
+    }
+
+    void flip()
+    {
+        Vector3 myScale = transform.localScale;
+        myScale.x *= -1;
+        transform.localScale = myScale;
+    }
 
 	void FixedUpdate(){
-		float horizontalMove = Input.GetAxis("Horizontal");
 
-		if(horizontalMove > 0){
-			Vector3 scale = playerModel.transform.localScale;
+        float horizontalMove;
+
+        if (wiimoteEnabled)
+        {
+            horizontalMove = wiiInput.getX();
+            anim.SetFloat("WalkSpeed", Mathf.Abs(horizontalMove));
+
+            if(wiiInput.getJump() > 0 && jumpCount == 0)
+            {
+                rigid.AddForce(Vector2.up * jumpForce);
+                jumpCount++;
+                Debug.Log("Jump "+jumpCount);
+            } else
+            {
+                jumpCount = 0;
+                Debug.Log("Jump " + jumpCount);
+            }
+
+        } else
+        {
+            horizontalMove = Input.GetAxis("Horizontal");
+            anim.SetFloat("WalkSpeed", Mathf.Abs(horizontalMove));
+        }
+
+        if(horizontalMove < 0 && !facingLeft)
+        {
+            flip();
+            facingLeft = true;
+            facingRight = false;
+        } else if(horizontalMove > 0 && !facingRight)
+        {
+            flip();
+            facingRight = true;
+            facingLeft = false;
+        }
+
+		/*if(horizontalMove > 0){
+			Vector3 scale = transform.localScale;
 			scale.x *= 1;
-			playerModel.transform.localScale = scale;
+			transform.localScale = scale;
 		} else {
-			Vector3 scale = playerModel.transform.localScale;
+			Vector3 scale = transform.localScale;
 			scale.x *= -1;
-			playerModel.transform.localScale = scale;
-		}
-
-		anim.SetFloat("WalkSpeed", Mathf.Abs(horizontalMove));
+			transform.localScale = scale;
+		}*/
+		
 
 		rigid.velocity = new Vector2(horizontalMove * moveSpeed, rigid.velocity.y);	
 	}
---
+++ Assets/Scripts/WiiMoteInputReader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WiiMoteInputReader : MonoBehaviour {
+
+    WiimoteReceiver receiver;
+
+    float nunchukX;
+    float nunchukY;
+    float wiimoteA;
+
+	void Start () {
+
+        Debug.Log("Running thangs!");
+        receiver = WiimoteReceiver.Instance;
+        receiver.connect();
+    }
+		
+	void Update () {         
+        Wiimote mote = receiver.wiimotes[1];
+        nunchukX = mote.NUNCHUK_JOY_X;
+        nunchukY = mote.NUNCHUK_JOY_Y;
+
+        wiimoteA = mote.BUTTON_A;
+
+        Debug.Log(nunchukX + ", " + nunchukY);        
+	}
+
+    public float getX() { return nunchukX; }
+    public float getY() { return nunchukY; }
+    public float getJump() { return wiimoteA; }
+}
