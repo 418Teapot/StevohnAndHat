bf63aa8fdd96831af448cc278bc4059420bd0a6f
robotto <mark@moore.dk>
2015-10-01 18:02:34 +0200

    calculating a 2D vector from enemy towards player

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -10,17 +10,27 @@ public class EnemyControl : MonoBehaviour {
     private Rigidbody2D rigid;
     private Transform enemyModel;
 
-    private GameObject player = GameObject.FindWithTag("Player");
+    private GameObject player;
+    private Rigidbody2D playerRigid;
+    private Vector2 towardsPlayer;
+    
 
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-        
+        player = GameObject.FindWithTag("Player");
+        playerRigid = player.GetComponent<Rigidbody2D>();
+        Debug.Log("enemy loaded:" + rigid);
+        Debug.Log("player is at:" + playerRigid.transform.position);
+        Debug.Log("enemy is at:" + rigid.transform.position);
+
     }
 
     // Update is called once per frame
-    void Update () {
-	    //Go to the player. Avoid things.
-	}
+    void FixedUpdate() {
+        towardsPlayer = playerRigid.position - rigid.position;
+        Debug.Log("2D vector towards player: " + towardsPlayer);
+        //Go to the player. Avoid things.
+    }
 }
