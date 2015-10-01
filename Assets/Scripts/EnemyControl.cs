ae127d9bab132e8753ce33585fb6a007aa6042db
robotto <mark@moore.dk>
2015-10-01 16:00:37 +0200

    Enemy assets and script (unfinished)

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+
+    // Use this for initialization
+    //public float moveSpeed = 5.0f;
+    //public float jumpForce = 15.0f;
+
+    private Rigidbody2D rigid;
+    private Transform enemyModel;
+
+    private GameObject player = GameObject.FindWithTag("Player");
+
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    //Go to the player. Avoid things.
+	}
+}
