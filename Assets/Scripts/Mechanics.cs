afedad52886c66e26fe88911adb377ad0d38e34b
Silas <Silas@Silass-MBP.lan>
2015-10-04 20:00:06 +0200

    beer

---
+++ Assets/Scripts/Mechanics.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mechanics : MonoBehaviour {
+	private int numberOfTables = 0;
+	public GameObject winZone;
+
+	// Use this for initialization
+	void Start () {
+		numberOfTables = GameObject.FindGameObjectsWithTag("Table").Length;
+		Debug.Log ("Tables" + numberOfTables);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(numberOfTables <= winZone.GetComponent<winCounter>().Tables()){
+			Debug.Log("you haz won");
+			Time.timeScale = 0f;
+		}
+	}
+}
---
+++ Assets/winCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class winCounter : MonoBehaviour {
+	private int tableCounter = 0;
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.tag == "Table"){
+			tableCounter ++;
+			Debug.Log ("Tables: " + tableCounter);
+		}
+
+	}
+
+	public int Tables(){
+		return tableCounter;
+	}
+}
