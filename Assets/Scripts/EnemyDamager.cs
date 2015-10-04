eb8fedfd368ab73e1707f1c8c12f1e8aacca77db
dkzeb <sthazeb@gmail.com>
2015-10-04 13:16:44 +0200

    Everything is good

---
+++ Assets/Scripts/EnemyDamager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamager : MonoBehaviour {
+
+    public GameObject deathParticles;
+    public int damage = 20;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<EnemyControl>().TakeDamage(damage);
+            Destroy(this.gameObject);   
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+	public float health = 10.0f;
+	public float recoverTime = 0.1f;
+	public float damage = 10.0f;
+
+	private float lastHitTime;
+	private PlayerControl playerControl;		// Reference to the PlayerControl script.
+	private GameObject camera;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		playerControl = GetComponent<PlayerControl>();
+		lastHitTime = Time.time;
+		Debug.Log("HP: " + health);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D (Collider2D col){
+		if(col.gameObject.tag == "Enemy"){
+			if (Time.time > lastHitTime + recoverTime){
+				health = health-damage;
+				Debug.Log("Damage taken: " + damage + " Health left: " + health );
+				if(health > 0f){
+					// ... take damage and reset the lastHitTime.
+					lastHitTime = Time.time; 
+				} else if (health <= 0.0f){
+					//die functions first
+					GetComponent<PlayerControl>().enabled = false;
+					foreach (CircleCollider2D cc in GetComponents<CircleCollider2D>()){
+						cc.enabled = false;
+					}
+					GameObject.FindWithTag("MainCamera").GetComponent<CameraFollow>().enabled = false;
+
+					//Respawn here, look under; IEnumerator Respawn()
+					StartCoroutine(Respawn());
+
+
+					Debug.Log("dead");
+				}
+			}
+		}	
+	}
+
+	IEnumerator Respawn() {
+		print(Time.time);
+		yield return new WaitForSeconds(2);
+		print(Time.time);
+		GetComponent<PlayerControl>().enabled = true;
+		foreach (CircleCollider2D cc in GetComponents<CircleCollider2D>()){
+			cc.enabled = true;
+		}
+		GameObject.FindWithTag("Player").transform.position = new Vector3(0,2,0);
+		GameObject.FindWithTag("MainCamera").GetComponent<CameraFollow>().enabled = true;
+
+	}
+
+}
