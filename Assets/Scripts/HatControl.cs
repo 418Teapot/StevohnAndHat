fbe4a0b0a35b2acd26ebb9a66601e1c1b7dc949a
dkzeb <sthazeb@gmail.com>
2015-10-04 11:23:35 +0200

    Hat yo!

---
+++ Assets/Scripts/HatControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HatControl : MonoBehaviour {
+
+    public GameObject hat;
+    public Transform muzzlePoint;
+    public float force = 100.0f;
+    
+	void FixedUpdate () {
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("FIRE!");
+            GameObject instantiatedHat = (GameObject) Instantiate(hat, muzzlePoint.position, muzzlePoint.rotation);
+            Rigidbody2D hatBody = instantiatedHat.GetComponent<Rigidbody2D>();
+            hatBody.AddForce(Vector2.right*force, ForceMode2D.Force);
+        }
+        
+    }
+}
