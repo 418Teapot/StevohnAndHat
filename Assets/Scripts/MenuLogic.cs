b66d727f966c5ce690f8973bf67645786381658d
dkzeb <sthazeb@gmail.com>
2015-10-05 20:48:01 +0200

    MenuZ man!

---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLogic : MonoBehaviour {
+
+	public void loadLevel()
+    {
+        Debug.Log("Loading...");
+        //GameObject.FindWithTag("MainCamera").GetComponent<ScreenFader>().EndScene(1);
+        Application.LoadLevel("daClub");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
